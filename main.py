320bed4c833bb896fc039e2c1520611536389d0d
Daniel Boa <daniel.boa@gmail.com>
2018-03-12 23:04:25 +0000

    Trying out recursive function loop.

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ from draw import draw_contrib_data
 
 user = os.environ['GITHUB_USER']
 access_token = os.environ['GITHUB_ACCESS_TOKEN']
-check_for_new_user_events = git.create_events_poller(user, access_token)
 
-contrib_data = git.get_user_contrib_data(user)
-draw_contrib_data(contrib_data)
+def loop():
+  check_for_new_user_events = git.create_events_poller(user, access_token)
+  contrib_data = git.get_user_contrib_data(user)
 
-# polling.poll(
-#   lambda: check_for_new_user_events(),
-#   step=60,
-#   poll_forever=True
-# )
+  draw_contrib_data(contrib_data)
+
+  polling.poll(
+    lambda: check_for_new_user_events(),
+    step=60,
+    poll_forever=True
+  )
+
+  loop()
