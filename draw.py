ca93089f4eed13bd0b46ce5e65977ce02393ad09
Daniel Boa <daniel.boa@gmail.com>
2018-03-12 21:36:28 +0000

    Wrote function to draw user contribution data to pimoroni scroll pHAT.

---
+++ draw.py
@@ -0,0 +1,21 @@
+import scrollphathd
+from scale import linear_scale
+
+def get_brightness_scale(contrib_data):
+  max_contrib_count = max([max(week) for week in weeks_to_draw])
+  domain = [0, max_contrib_count]
+  return linear_scale(domain, [0, 1])
+
+def draw_contrib_data(contrib_data):
+  (width, height) = scrollphathd.get_buffer_shape()
+  first_day_index = len(contrib_data) - width
+  weeks_to_draw = contrib_data[first_day_index:]
+  brightness_scale = get_brightness_scale(weeks_to_draw)
+
+  for x in range(width):
+    for y in range(7):
+      contrib_count = weeks_to_draw[x][y]
+      brightness = brightness_scale(contrib_count)
+      scrollphathd.set_pixel(x, y, brightness)
+
+  scrollphathd.show()
--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 import os
 import polling
 import git
+from draw import draw_contrib_data
 
 user = os.environ['GITHUB_USER']
 access_token = os.environ['GITHUB_ACCESS_TOKEN']
 check_for_new_user_events = git.create_events_poller(user, access_token)
 
-git.get_user_contrib_data(user)
+contrib_data = git.get_user_contrib_data(user)
+draw_contrib_data(contrib_data)
 
 # polling.poll(
 #   lambda: check_for_new_user_events(),
